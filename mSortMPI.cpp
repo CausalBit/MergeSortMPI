68f55c40b2bd03637a5bce0a5a991c2fc3905c7f
Irvin Umaña <irvin.umana@ucr.ac.cr>
2017-09-03 12:08:11 -0600

    added necessary barriers

--- mSortMPI.cpp
+++ mSortMPI.cpp
@@ -76,50 +76,95 @@ int main( int argc, char *argv[] )
       r = n % p;  //Restante de la division de items en la lista entre los procesos. 
                   //También indica los r primero procesos a recibir t+1 items.  
       t = (n - r)/p;  //Cuantos items por proceso para que tengan el mismo número de procesos (no contando los restantes).
+      int num_elementos_to_send = n - (2*r);
 
-      lista_cantidadMenor = lista+r;
+      lista_cantidadMenor = lista+2*r;
+
+      for(int i = 0; i < num_elementos_to_send; i++){
+        std::cout <<"tesnting lista cantidad menor: " << lista_cantidadMenor[i] << std::endl;
+      }
 
       if(r != 0){
-      MPI_Send (lista_cantidadMenor,n-r,MPI_INT,r,cantidadMenorTag, MPI_COMM_WORLD);  //Mandamos la lista de cantidades menores al pocesador que se convertira
+      std::cout << "About to send lista_cantidadMenor to process " << r << std::endl; 
+      //MPI_Send (&buf,count,datatype,dest,tag,comm) 
+      MPI_Send (lista_cantidadMenor,num_elementos_to_send,MPI_INT,r,cantidadMenorTag, MPI_COMM_WORLD);  //Mandamos la lista de cantidades menores al pocesador que se convertira
                                                                                       //en la raiz de su comunicador que se crea al hacer el split después.
       std::cout << "0 is done sending lista_cantidadMenor" << std::endl; 
       }
-      free(lista_cantidadMenor);
+
+      //free(lista_cantidadMenor);
+      lista_cantidadMenor = NULL;
     }
-	         
+	       
+
     MPI_Bcast(&t, 1, MPI_INT, 0, MPI_COMM_WORLD);
    	MPI_Bcast(&r, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD); 
+    if(my_rank == 0 ){std::cout << "Donde broadcasting with r = "<< r << std::endl; }
 
-    if(my_rank == r-1)//Soy el proceso raíz del segundo subgrupo de comm. 
+
+
+    if(my_rank == r)//Soy el proceso raíz del segundo subgrupo de comm. 
     {
-      MPI_Recv(lista,n-r,MPI_INT,r-1,cantidadMenorTag,MPI_COMM_WORLD,MPI_STATUS_IGNORE); //Recibir la lista de cantidades menores
+      std::cout << "preparing list with n = " << n << std::endl;
+      int num_elementos_to_rcv = n - 2*r;
+
+      lista = (int *)malloc(num_elementos_to_rcv*sizeof(int));
+          if(lista==NULL){
+            std::cout << "Error al asignar memoria a la lista" << endl;
+            return 0; 
+          }
+      std::cout << my_rank <<  " is bout to receive " << std::endl;
+      //MPI_Recv (&buf,count,datatype,source,tag,comm,&status) 
+      MPI_Recv(lista,n-2*r,MPI_INT,0,cantidadMenorTag,MPI_COMM_WORLD,MPI_STATUS_IGNORE); //Recibir la lista de cantidades menores
+      std::cout << "Done receiving, starting testing: ";
+      for(int i = 0; i < n - 2*r; i++){
+        std::cout << "--" << lista[i] << std::endl;
+      } 
+      std::cout << "la lista está terminada" <<std::endl;
     }
+     
     
 
     //Vamos a dividir los procesos en dos sub comunicadores. El de color 1 que recibe t+1 items en sus listas. 
-    //El de color 2 que recibe t items. Esto para garantizar una manera de recibir cualquier cantidad de n de items. 
+    //El de color 2 que recibe t items. Esto para garantizar una manera de recibir cualquier cantidad de n de items.
+
+   // std::cout << "Starting to calculate color for "<< my_rank <<std::endl; 
 
     int color = my_rank < r ? 1 : 0; //Esto es para calcular el color (identificar procesos de un comunicador) del proceso.
+    std::cout << " pro "<< my_rank << "with color "<< color << std::endl; 
+    MPI_Barrier(MPI_COMM_WORLD);
 
     //Aquí es donde se reparte los procesos según color y se identifican con el COMM handler balancer_comm. 
     MPI_Comm_split(MPI_COMM_WORLD, color, my_rank, &balancer_comm);
+    std::cout << "After Splitiing about to allocate "<< my_rank << " with balancer: "<< balancer_comm <<std::endl; 
 
     //Ahora, dependiendo del color, la lista será del tamaño t+1, o t.
     lista_local = (int *)malloc((t+color)*sizeof(int));
+     std::cout << "Done allocating "<< my_rank <<std::endl; 
+
+     MPI_Barrier(MPI_COMM_WORLD);
 
     if(r > 0){
       //Vamos a repartir la lista. Para los procesos en COMM_WORLD con rank menor a r, esto reciben t+1, para los otro t. 
-      MPI_Scatter(lista, t + 1, MPI_INT, lista_local, t + 1, MPI_INT, 0, balancer_comm);
-      MPI_Scatter(lista, t, MPI_INT, lista_local, t, MPI_INT, r-1, balancer_comm);
+       // MPI_Scatter (&sendbuf,sendcnt,sendtype,&recvbuf,recvcnt,recvtype,root,comm)
+      
+        MPI_Scatter(lista, t + 1, MPI_INT, lista_local, t + 1, MPI_INT, 0, balancer_comm);
+      
+    
+       
+      MPI_Scatter(lista, t, MPI_INT, lista_local, t, MPI_INT, 0, balancer_comm);
+
+      
     }else{
       lista_local = lista;
     }
 
-
-  std::cout << "Yo soy el proceso " << my_rank << " del comunicador: "<< balancer_comm << " first: " << lista_local[0] << std::endl;
+MPI_Barrier(MPI_COMM_WORLD);
+  std::cout << "Yo soy el proceso " << my_rank <<  " first: " << lista_local[0] << std::endl;
   
-  if(my_rank == 0 || my_rank == r-1){
+  if(my_rank == 0 || my_rank == r){
 	   free(lista);
   }
   
